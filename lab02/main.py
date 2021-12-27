fd72326344c48a0fc4784b4f78a4d38464a7b670
TheYankeee <slavan01@list.ru>
2021-12-27 21:51:06 +0300

    Done lab02

---
+++ lab02/main.py
@@ -0,0 +1,18 @@
+from lab_python_oop.square import Square
+from lab_python_oop.circle import Circle
+from lab_python_oop.rect import Rect
+
+
+def main():
+    r = Rect(2, 3, *(100, 0, 200))
+    c = Circle(5, *(0, 200, 100))
+    s = Square(4, *(200, 100, 0))
+    print(r)
+    print(c)
+    print(s)
+
+    print('Фигуры:\n{}\n{}\n{}'.format(s, c, r))
+
+
+if __name__ == "__main__":
+    main()
